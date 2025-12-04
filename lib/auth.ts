ed0c658507ff729604d3d10cb6fd0350100f833a
Habib Charter PC <habibjutt868@gmail.com>
2025-12-04 17:55:57 +0400

    shadcn and prisma

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+// import { PrismaClient } from "@/generated/prisma/client";
+import db from "./db";
+
+// const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+});
---
+++ prisma/migrations/20251204134837_init_accounts/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
